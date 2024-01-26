792eec0bbc7cece8d82597b5f8a51003e995fe1a
Emmanuel <117133255+edeni4458@users.noreply.github.com>
2024-01-26 18:16:33 -0500

    Add files via upload

---
+++ Basics III/meaning.py
@@ -0,0 +1,11 @@
+meaning = 42
+print('')
+
+# if meaning > 10:
+#     print('Corrrrrect!')
+# else:
+#     print('Wrrrrong again!')
+
+# Ternary Operator
+
+print("Correct") if meaning > 10 else print("Wrong bra.")

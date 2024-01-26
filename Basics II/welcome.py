06a4551ec6b2eca02eeb1c5716f910e18ddc7635
Emmanuel <117133255+edeni4458@users.noreply.github.com>
2024-01-26 18:15:32 -0500

    Add files via upload

---
+++ Basics II/welcome.py
@@ -0,0 +1,11 @@
+line_01 = "********************" # header / footer
+line_02 = "*                  *"
+line_03 = "*     Welcome!     *"
+
+print('')
+
+print(line_01)
+print(line_02)
+print(line_03)
+print(line_02)
+print(line_01)
